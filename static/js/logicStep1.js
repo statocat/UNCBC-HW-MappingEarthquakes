4f62653c247ea43c330ede955b1de7f2fbacd554
statocat <catherineesmith1415@gmail.com>
2022-12-13 00:40:30 -0500

    :)

--- static/js/logicStep1.js
+++ static/js/logicStep1.js
@@ -16,51 +16,54 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/
 
 // Create map style options menu and add to map
 let baseMaps = {
-  "Streets": streets,
-  'Satellite Streets': satelliteStreets
+  'Streets': streets,
+  'Satellite': satelliteStreets
 };
 
-// Create the map object with center and zoom level.
+// Create the map object with center of US and zoom level.
 let map = L.map('mapid', {
-  center: [43.7,-79.3],
-  zoom:11,
+  center: [39.5,-98.5],
+  zoom:3,
   layers:[streets]
 });
 
 // Create controller to select map style option
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/statocat/UNCBC-HW14-MappingEarthquakes/MappingGeoJSONPolygons/static/js/torontoNeighborhoods.json";
-
-// // Create a style for the lines.
-let myStyle = {
-  color: "blue",
-  weight: 1,
-  fillColor: "yellow"
-}
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-  L.geoJSON(data, {
-    style: myStyle,
-    onEachFeature: function(feature, layer){
-      layer.bindPopup(
-        "<h3> Neighborhood: "+feature.properties.AREA_NAME+"</h3>"
-      )
-    }
-  }).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
 
-  // , 
-  //   {
-  //   style: myStyle,
-  //   onEachFeature: function(feature, layer){
-  //     layer.bindPopup("<h3> Airline: "+
-  //     feature.properties.airline +
-  //     "</h3> <hr> <h3> Destination: " +
-  //     feature.properties.dst +
-  //     "</h3>");
-  //   }
-  // }
+// // // Create a style for the lines.
+// let myStyle = {
+//   color: "blue",
+//   weight: 1,
+//   fillColor: "yellow"
+// }
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+//   L.geoJSON(data, {
+//     style: myStyle,
+//     onEachFeature: function(feature, layer){
+//       layer.bindPopup(
+//         "<h3> Neighborhood: "+feature.properties.AREA_NAME+"</h3>"
+//       )
+//     }
+//   }).addTo(map);
+// });
+
+//   // , 
+//   //   {
+//   //   style: myStyle,
+//   //   onEachFeature: function(feature, layer){
+//   //     layer.bindPopup("<h3> Airline: "+
+//   //     feature.properties.airline +
+//   //     "</h3> <hr> <h3> Destination: " +
+//   //     feature.properties.dst +
+//   //     "</h3>");
+//   //   }
+//   // }
