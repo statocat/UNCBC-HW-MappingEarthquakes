c5a0dca154cefd808491bd8dcaf42eb030a244a9
statocat <catherineesmith1415@gmail.com>
2022-12-13 01:55:37 -0500

    :)

--- static/js/logicStep4.js
+++ static/js/logicStep4.js
@@ -20,6 +20,15 @@ let baseMaps = {
   'Satellite': satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays ={
+    Earthquakes: earthquakes
+};
+
 // Create the map object with center of US and zoom level.
 let map = L.map('mapid', {
   center: [39.5,-98.5],
@@ -27,8 +36,9 @@ let map = L.map('mapid', {
   layers:[satelliteStreets]
 });
 
-// Create controller to select map style option
-L.control.layers(baseMaps).addTo(map);
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json( "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
@@ -89,5 +99,33 @@ function getColor(magnitude) {
     onEachFeature: function(feature, layer) {
         layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>" + "Location: " + feature.properties.place);
     }
-      }).addTo(map);
+      }).addTo(earthquakes);
 });
+
+earthquakes.addTo(map)
+
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+//   L.geoJSON(data, {
+//     style: myStyle,
+//     onEachFeature: function(feature, layer){
+//       layer.bindPopup(
+//         "<h3> Neighborhood: "+feature.properties.AREA_NAME+"</h3>"
+//       )
+//     }
+//   }).addTo(map);
+// });
+
+//   // , 
+//   //   {
+//   //   style: myStyle,
+//   //   onEachFeature: function(feature, layer){
+//   //     layer.bindPopup("<h3> Airline: "+
+//   //     feature.properties.airline +
+//   //     "</h3> <hr> <h3> Destination: " +
+//   //     feature.properties.dst +
+//   //     "</h3>");
+//   //   }
+//   // }
