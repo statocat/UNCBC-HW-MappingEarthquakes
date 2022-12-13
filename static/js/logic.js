c3883fffb112db0e3a20836aeb18d69240d96596
statocat <catherineesmith1415@gmail.com>
2022-12-12 23:53:52 -0500

    :)

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,8 @@
 // Create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v12',
+    id: 'mapbox/light-v11',
     accessToken: API_KEY
 });
 
@@ -13,34 +13,58 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
     id: 'mapbox/dark-v11',
     accessToken: API_KEY
 });
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/navigation-day-v1',
+    accessToken: API_KEY
+});
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/navigation-night-v1',
+    accessToken: API_KEY
+});
 
 // Create map style options menu and add to map
 let baseMaps = {
-  Street: streets,
-  Dark: dark
+  Light: light,
+  Dark: dark,
+  Day: day,
+  Night: night
 }
 
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+let map = L.map('mapid', {
+  center: [44,-80],
+  zoom:2,
+  layers:[night]
+})
 
 // Create controller to select map style option
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/statocat/UNCBC-HW14-MappingEarthquakes/MappingGeoJSONPoints/static/js/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/statocat/UNCBC-HW14-MappingEarthquakes/MappingGeoJSONPoints/static/js/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
   
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
+    style: myStyle,
     onEachFeature: function(feature, layer){
-      layer.bindPopup("<h2>" + 
-      "Airport Code: "+
-      feature.properties.faa +
-      "</h2> <hr> <h3>Airport Name: " +
-      feature.properties.name +
+      layer.bindPopup("<h3> Airline: "+
+      feature.properties.airline +
+      "</h3> <hr> <h3> Destination: " +
+      feature.properties.dst +
       "</h3>");
     }
   }).addTo(map)
