4e733956bf7b5d7b961c275b9615c8e64dfdeb2b
statocat <catherineesmith1415@gmail.com>
2022-12-13 00:29:23 -0500

    :)

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,71 +1,66 @@
 // Create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/light-v11',
+    id: 'mapbox/streets-v12',
     accessToken: API_KEY
 });
 
 // Create the tile layer that will be the background of our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v11',
-    accessToken: API_KEY
-});
-let day = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/navigation-day-v1',
-    accessToken: API_KEY
-});
-let night = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/navigation-night-v1',
+    id: 'mapbox/satellite-streets-v12',
     accessToken: API_KEY
 });
 
 // Create map style options menu and add to map
 let baseMaps = {
-  Light: light,
-  Dark: dark,
-  Day: day,
-  Night: night
-}
+  "Streets": streets,
+  'Satellite Streets': satelliteStreets
+};
 
 // Create the map object with center and zoom level.
 let map = L.map('mapid', {
-  center: [44,-80],
-  zoom:2,
-  layers:[night]
-})
+  center: [43.7,-79.3],
+  zoom:11,
+  layers:[streets]
+});
 
 // Create controller to select map style option
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/statocat/UNCBC-HW14-MappingEarthquakes/MappingGeoJSONPoints/static/js/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/statocat/UNCBC-HW14-MappingEarthquakes/MappingGeoJSONPolygons/static/js/torontoNeighborhoods.json";
 
-// Create a style for the lines.
+// // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: "blue",
+  weight: 1,
+  fillColor: "yellow"
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
-  
-  // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
     style: myStyle,
     onEachFeature: function(feature, layer){
-      layer.bindPopup("<h3> Airline: "+
-      feature.properties.airline +
-      "</h3> <hr> <h3> Destination: " +
-      feature.properties.dst +
-      "</h3>");
+      layer.bindPopup(
+        "<h3> Neighborhood: "+feature.properties.AREA_NAME+"</h3>"
+      )
     }
-  }).addTo(map)
-}); 
+  }).addTo(map);
+});
+
+  // , 
+  //   {
+  //   style: myStyle,
+  //   onEachFeature: function(feature, layer){
+  //     layer.bindPopup("<h3> Airline: "+
+  //     feature.properties.airline +
+  //     "</h3> <hr> <h3> Destination: " +
+  //     feature.properties.dst +
+  //     "</h3>");
+  //   }
+  // }
